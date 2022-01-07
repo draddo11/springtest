45686560e9e45e02cadbb9f7a2364f85ded5cd19
draddo11 <38097150+draddo11@users.noreply.github.com>
2022-01-07 23:18:19 +0000

    created files

---
+++ src/main/java/com/nana/springtest/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.nana.springtest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public @ResponseBody
+    String greeting() {
+        return "Dummy Project for testing";
+    }
+
+}
---
+++ src/test/java/com/nana/springtest/SpringtestApplicationTest.java
@@ -0,0 +1,12 @@
+package com.nana.springtest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpringtestApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
