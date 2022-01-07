c71e8c37ab4975c491111f5b8d21995ef6742fa0
draddo11 <38097150+draddo11@users.noreply.github.com>
2022-01-07 22:52:10 +0000

    initial commit

---
+++ src/test/java/com/nana/springtest/SpringtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nana.springtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
